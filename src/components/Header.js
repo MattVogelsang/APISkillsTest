d6a76ce507f2abc4b0735d222386257e5727554a
MattVogelsang <vogelsangmatt@gmail.com>
2025-12-02 14:29:36 -0500

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,12 +13,7 @@ function Header() {
           Overview
         </button>
         <button className="nav-item active">
-          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/>
-            <circle cx="9" cy="7" r="4"/>
-            <path d="M23 21v-2a4 4 0 0 0-3-3.87"/>
-            <path d="M16 3.13a4 4 0 0 1 0 7.75"/>
-          </svg>
+          <img src="/images/group_FILL0_wght300_GRAD0_opsz24.svg" alt="Patients" className="nav-icon" />
           Patients
         </button>
         <button className="nav-item">
