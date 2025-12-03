9e37aa254e9c4b75c35e881425939423740eee56
MattVogelsang <vogelsangmatt@gmail.com>
2025-12-02 22:27:08 -0500

    .

--- src/components/BloodPressureChart.js
+++ src/components/BloodPressureChart.js
@@ -6,9 +6,6 @@ function BloodPressureChart({ chartData }) {
   const options = {
     responsive: true,
     maintainAspectRatio: false,
-    animation: {
-      duration: 0
-    },
     plugins: {
       legend: { display: false },
       tooltip: { mode: 'index', intersect: false }
